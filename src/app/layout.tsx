c710b2a2ef2b51b3e17d3747e540a2a59cf6eceb
Nanford <843395729@qq.com>
2025-03-25 17:30:15 +0800

    新增icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,10 +21,14 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <GoogleAnalytics />
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-        <GoogleAnalytics />
+        {/* 网站图标 */}
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" href="/icon.svg" type="image/svg+xml" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        <link rel="manifest" href="/manifest.json" />
       </head>
       <body className={inter.className}>
         <Providers>
